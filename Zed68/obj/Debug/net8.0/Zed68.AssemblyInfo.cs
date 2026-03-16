e2df32ba48a3272d094a0c8bef0073b5383054c3
thealicejane <thealicejane@outlook.com>
2026-03-16 17:26:04 +0000

    opened in visual studio
    idk what changed but something did

--- Zed68/obj/Debug/net8.0/Zed68.AssemblyInfo.cs
+++ Zed68/obj/Debug/net8.0/Zed68.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zed68")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ab16fa02124a608bddac1e82cef140418a62658")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zed68")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zed68")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
